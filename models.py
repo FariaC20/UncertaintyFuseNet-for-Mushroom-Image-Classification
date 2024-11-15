7b1e2942630d017abc577bd742d9725cf58dc8ad
FariaC20 <117250745+FariaC20@users.noreply.github.com>
2024-11-15 23:34:51 +0600

    Update models.py

--- models.py
+++ models.py
@@ -29,9 +29,9 @@ class ImageClassifierBase:
             for i, layer in enumerate(model.layers):
                 layer.set_weights(self.trained_model.layers[i].get_weights())
 
-        callbacks = None if self.trunc else self._get_callbacks()
+        callback = None if self.trunc else self._get_callbacks()
 
-        return model, callbacks
+        return model, callback
 
     def _compile_model(self, model):
         adam = tf.keras.optimizers.Adam(lr=self.lr)
