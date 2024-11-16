f6e259d113918d030956d6dc7732ad7c6c79a4c7
FariaC20 <117250745+FariaC20@users.noreply.github.com>
2024-11-16 18:27:06 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from collections import Counter
+from sklearn.utils import shuffle
 
 def rgb2gray(rgb):
     return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
