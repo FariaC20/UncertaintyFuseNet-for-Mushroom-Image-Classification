b5090b8c4fcf6911bd356ec57276d830bad9e176
FariaC20 <117250745+FariaC20@users.noreply.github.com>
2024-11-17 10:20:06 +0600

    Update models.py

--- models.py
+++ models.py
@@ -176,9 +176,11 @@ class SimpleCNNModel(tf.keras.Model):
         x = Flatten()(concatenated_features)
 
         x = Dense(units=128, activation='relu')(x)
-  def compile(self, **kwargs): # use compile for defining the optimizer
-    super(SimpleCNNModel, self).compile(**kwargs) # use compile for defining the optimizer
-    self.mc_dropout_enabled = False # by default, it is turned off
+        
+    def compile(self, **kwargs): # use compile for defining the optimizer
+        super(SimpleCNNModel, self).compile(**kwargs) # use compile for defining the optimizer
+        self.mc_dropout_enabled = False # by default, it is turned off
+        
         if not self.trunc:
             x = self._get_dropout(x, rate=0.7)
             x = Dense(units=64, activation='relu')(x)
