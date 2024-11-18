37de394fc15ebddfe80e9088310a3656c94220d0
FariaC20 <117250745+FariaC20@users.noreply.github.com>
2024-11-18 18:21:10 +0600

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Lambda
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization, Concatenate
